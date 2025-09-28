f9d05fb6c3ccf3ea2d30201e63680a0f0622bb70
Alexis Belmonte <alexbelm48@gmail.com>
2025-09-28 17:19:21 -0400

    [META] [UPDATE] Update submodule pointers
    This is an automated commit. The following submodules were updated:

--- controller.py
+++ controller.py
@@ -9,53 +9,53 @@ import subprocess
 
 class Controller:
     DEV_ENVIRONMENT_TIMEOUT = 60
+    DEV_ENVIRONMENT_SUBMODULE_NAME = "dev-environment"
 
     def __init__(self):
-        self._repository = Repo(
-            os.path.join(
-                os.path.abspath(
-                    os.path.dirname(sys.argv[0])
-                ), ".git"
-            )
-        )
+        self._runtime_path = os.path.abspath(os.path.dirname(sys.argv[0]))
+        self._repository_path = os.path.abspath(os.path.join(self._runtime_path, ".git"))
+
+        self._repository = Repo(self._repository_path)
 
         self.update()
 
     def _update_repositories(self):
         L.debug("Pulling changes from environment repository")
 
+        env_main_module = self._repository.submodule(self.DEV_ENVIRONMENT_SUBMODULE_NAME)
+
         try:
-            origin = self._repository.remote(name='origin')
-            origin.pull()
+            env_main_module.update(to_latest_revision=True, init=True)
         except Exception as e:
             L.error(f"Failed to update environment repository, giving up: {e}")
             return
 
-        L.debug("Pulling changes from {} repository submodules".format(len(self._repository.submodules)))
+        env_submodules = env_main_module.module().submodules
+        L.debug("Pulling {} submodules from environment repository".format(len(env_submodules)))
 
-        submodule_update_list = []
+        env_submodule_list = []
 
-        for submodule in self._repository.submodules:
+        for env_submodule in env_submodules:
             try:
-                submodule.update(to_latest_revision=True, init=True)
+                env_submodule.update(to_latest_revision=True, init=True)
             except Exception as e:
-                L.error(f"Failed to update submodule {submodule.name}, skipping: {e}")
+                L.error(f"Failed to update submodule {env_submodule.name}, skipping: {e}")
                 continue
 
-            if submodule.module().head.commit.hexsha == submodule.hexsha:
+            if env_submodule.module().head.commit.hexsha == env_submodule.hexsha:
                 continue
 
-            submodule_update_list.append(submodule)
+            env_submodule_list.append(env_submodule)
 
-        submodule_update_list = [
+        env_submodule_list = [
             "- {}: commit {} ({})".format(
                 submodule.name,
                 submodule.module().head.commit.hexsha,
                 submodule.module().head.commit.message.splitlines()[0]
-            ) for submodule in submodule_update_list
+            ) for submodule in env_submodule_list
         ]
 
-        L.info("Updated {} submodules".format(len(submodule_update_list)))
+        L.info("Updated {} submodules".format(len(env_submodule_list)))
         L.debug("Pushing repository submodule pointer updates")
 
         try:
@@ -65,7 +65,7 @@ class Controller:
 [META] [UPDATE] Update submodule pointers
 
 This is an automated commit. The following submodules were updated:
-{"\n".join(submodule_update_list)}
+{"\n".join(env_submodule_list)}
 """.strip())
                 origin.push()
         except Exception as e:
